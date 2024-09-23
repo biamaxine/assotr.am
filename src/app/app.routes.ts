3b03551c98ab341c209bba9f461effe84ef64686
biamaxine <bianca.maxine.dev@gmail.com>
2024-09-23 17:35:48 -0400

    feat: ADD Components [Home, Event]

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import {Routes} from '@angular/router';
+import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { EventComponent } from './views/event/event.component';
+import { HomeComponent } from './views/home/home.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'event/:id', component: EventComponent },
+];
